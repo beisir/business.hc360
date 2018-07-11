3ecb86235648fa091a7c90ecc20d5fea8b2f9c4d
beisir <hsy7426@163.com>
2018-07-11 10:15:19 +0800

    合并代码

--- src/main.js
+++ src/main.js
@@ -3,19 +3,25 @@ import Router from 'vue-router';
 import http from '@/util/http'
 import App from './App';
 import routerConfig from './router';
-import {Menu, Submenu, MenuItem} from 'element-ui';
+import {Menu, Submenu, MenuItem, Steps, Step, Input, Form, Button, ButtonGroup, FormItem, Checkbox} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '../static/iconfont/css/font-awesome.min.css';
-
 Vue.use(Menu);
 Vue.use(Submenu);
 Vue.use(MenuItem);
 Vue.use(Router);
+Vue.use(Steps);
+Vue.use(Step);
+Vue.use(Input);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Button);
+Vue.use(ButtonGroup);
+Vue.use(Checkbox);
 Vue.use(http);
-
 const router = routerConfig(Router);
-Vue.config.productionTip = false;
 
+Vue.config.productionTip = false;
 new Vue({
     el: '#app',
     router,
